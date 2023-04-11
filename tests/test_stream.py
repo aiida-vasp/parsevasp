d6f9922aed35c8d43ff5199e6624d968cfd2e012
Espen Flage-Larsen <espen.flage-larsen@sigma2.no>
2023-04-11 16:45:26 +0200

    Added inverse logic for triggers and changed to nostart

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,25 +7,35 @@ import pytest
 from parsevasp.stream import Stream
 
 
-@pytest.fixture(scope='module', params=[0])
+@pytest.fixture()
 def stream_parser(request, tmpdir_factory):
     """Load a stream.
 
     """
+    try:
+        name = request.param
+    except AttributeError:
+        # Test not parametrized
+        name = 'stdout'
     testdir = os.path.dirname(__file__)
-    stream_file = testdir + '/stdout'
+    stream_file = testdir + '/' + name
     stream = Stream(file_path=stream_file)
 
     return stream
 
 
-@pytest.fixture(scope='module', params=[0])
+@pytest.fixture()
 def stream_parser_file_objects(request, tmpdir_factory):
     """Load stream file from a file object.
 
     """
+    try:
+        name = request.param
+    except AttributeError:
+        # Test not parametrized
+        name = 'stdout'
     testdir = os.path.dirname(__file__)
-    stream_file = testdir + '/stdout'
+    stream_file = testdir + '/' + name
     stream = None
     with open(stream_file) as file_handler:
         stream = Stream(file_handler=file_handler)
@@ -40,18 +50,32 @@ def test_stream(stream_parser):
 
     entries = stream_parser.entries
     assert stream_parser.configured_streams
-    assert stream_parser.number_of_entries == 2
+    print(entries)
+    assert stream_parser.number_of_entries == 1
     assert stream_parser.has_entries
-    assert str(entries[1]) == '(ERROR) ibzkpt: Error with the k-points.'
+    assert str(entries[0]) == '(ERROR) ibzkpt: Error with the k-points.'
 
 
+def test_stream_objects(stream_parser_file_objects):
+    """Check if stream_parser_file_objects returns expected results passing an object
+
+    """
+
+    entries = stream_parser_file_objects.entries
+    assert stream_parser_file_objects.configured_streams
+    assert stream_parser_file_objects.number_of_entries == 1
+    assert stream_parser_file_objects.has_entries
+    assert str(entries[0]) == '(ERROR) ibzkpt: Error with the k-points.'
+
+
+@pytest.mark.parametrize('stream_parser', (['stdout_nostart']), indirect=['stream_parser'])
 def test_executed(stream_parser):
     """Check if stream_parser returns expected results for execution checks.
 
     """
 
     entries = stream_parser.entries
-    assert entries[0].shortname == 'started'
+    assert entries[0].shortname == 'nostart'
 
 
 def test_stream_override(stream_parser):
@@ -72,10 +96,10 @@ def test_stream_override(stream_parser):
             }
         }
     )
-    assert len(stream.entries) == 2
-    assert stream.entries[1].kind == 'WARNING'
-    assert stream.entries[1].regex == re.compile('internal error')
-    assert stream.entries[1].message == 'some error'
-    assert stream.entries[1].suggestion == 'none'
-    assert stream.entries[1].location == stream_parser.entries[1].location
-    assert stream.entries[1].recover == stream_parser.entries[1].recover
+    assert len(stream.entries) == 1
+    assert stream.entries[0].kind == 'WARNING'
+    assert stream.entries[0].regex == re.compile('internal error')
+    assert stream.entries[0].message == 'some error'
+    assert stream.entries[0].suggestion == 'none'
+    assert stream.entries[0].location == stream_parser.entries[0].location
+    assert stream.entries[0].recover == stream_parser.entries[0].recover
